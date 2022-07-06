74a3fa1fb631813773613dfa0631d549f29d00c4
CarlosAugustoPrado <carlosaugusto_prado@yahoo.com.br>
2022-07-06 11:44:14 -0300

    Criação do menu fixo em JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,18 @@ function openDropdown(event) {
   dropdown.classList.toggle('active');
 }
 
-
 btnDropdown.addEventListener('click', openDropdown);
-
 dropdown.addEventListener('mouseleave', openDropdown);
+
+
+const header = document.getElementById('js-header')
+
+function fixedMenu() {
+  if(window.pageYOffset > 80) {
+    header.classList.add('fixed-menu');  
+  } else {
+    header.classList.remove('fixed-menu');
+  }
+}
+
+document.addEventListener('scroll', fixedMenu);
